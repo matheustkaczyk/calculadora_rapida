6198a062a6937732db556431b2cbd0f7075b08f0
Matheus Tkaczyk <matheus111904@hotmail.com>
2024-08-06 13:56:20 -0300

    commit bombinha

--- lib/components/checkout_modal.dart
+++ lib/components/checkout_modal.dart
@@ -5,6 +5,7 @@ import '../state/total_sum_provider.dart';
 class CheckoutModal {
   static void showModal(BuildContext context, WidgetRef ref) {
     final TextEditingController paidValueController = TextEditingController();
+    final FocusNode paidValueFocusNode = FocusNode();
     final totalSum = ref.read(totalSumProvider);
     double change = 0.0;
 
@@ -12,6 +13,10 @@ class CheckoutModal {
       context: context,
       isScrollControlled: true,
       builder: (BuildContext context) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          paidValueFocusNode.requestFocus();
+        });
+
         return Padding(
           padding: EdgeInsets.only(
             bottom: MediaQuery.of(context).viewInsets.bottom,
@@ -20,52 +25,53 @@ class CheckoutModal {
             child: Container(
               padding: const EdgeInsets.all(16.0),
               child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text(
-                        "Checkout",
-                        style: Theme.of(context).textTheme.headlineMedium,
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Checkout",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Total: R\$ ${totalSum.toStringAsFixed(2)}',
+                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    TextField(
+                      controller: paidValueController,
+                      focusNode: paidValueFocusNode,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor pago',
+                        border: OutlineInputBorder(),
                       ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Total: R\$ ${totalSum.toStringAsFixed(2)}',
-                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        controller: paidValueController,
-                        decoration: const InputDecoration(
-                          labelText: 'Valor pago',
-                          border: OutlineInputBorder(),
-                        ),
-                        keyboardType: TextInputType.number,
-                        onChanged: (value) {
-                          final paidAmount = double.tryParse(value) ?? 0.0;
-                          change = paidAmount - totalSum;
-                        },
-                      ),
-                      const SizedBox(height: 20),
-                      Divider(),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Troco: R\$ ${change.toStringAsFixed(2)}',
-                        style: const TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text('Fechar'),
-                      ),
-                    ],
-                  ),
+                      keyboardType: TextInputType.number,
+                      onChanged: (value) {
+                        final paidAmount = double.tryParse(value) ?? 0.0;
+                        change = paidAmount - totalSum;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Divider(),
+                    const SizedBox(height: 10),
+                    Text(
+                      'Troco: R\$ ${change.toStringAsFixed(2)}',
+                      style: const TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Fechar'),
+                    ),
+                  ],
                 ),
               ),
             ),
-          );
+          ),
+        );
       },
     );
   }
